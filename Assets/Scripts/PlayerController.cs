b8d119eb6462be9817b6cc3f72ddf8cf711a920e
Justin Moore <theoperaking@yahoo.com>
2020-07-08 19:38:06 -0400

    Add player and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform player;
+    public float speed;
+    public float maxBound, minBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (player.position.x < minBound && h < 0) h = 0;
+        else if (player.position.x > maxBound && h > 0) h = 0;
+
+        player.position += Vector3.right * h * speed;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+The tutorial this repo is based on:
+https://www.youtube.com/watch?v=cnfwNzpoIlA
