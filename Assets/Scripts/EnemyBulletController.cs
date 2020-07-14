83633ea9226d777ad3bf8a9e2554c72d2e930e3f
Justin Moore <theoperaking@yahoo.com>
2020-07-13 20:50:08 -0400

    Add enemy and enemy bullets

---
+++ Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        bullet.position += Vector3.up * -speed;
+
+        if (bullet.position.y <= -10) Destroy(bullet.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            GameOver.IsPlayerDead = true;
+        }
+
+        else if(collision.tag == "Base")
+        {
+            GameObject playerBase = collision.gameObject;
+            BaseHealth baseHealth = playerBase.GetComponent<BaseHealth>();
+            baseHealth.health -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
